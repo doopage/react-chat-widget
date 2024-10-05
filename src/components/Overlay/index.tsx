6bc592083598f18bd13651d5864c35bf211c2463
Nobi <nobi@nobidev.com>
2024-10-05 10:42:29 +0700

    Add background color and opacity props to Overlay component

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -1,15 +1,19 @@
 export type Props = {
   zIndex?: number;
   onClick?: () => void;
+  backgroundColor?: string;
+  opacity?: number;
 }
 
-function Overlay({ zIndex = 1_000, onClick }: Props) {
+function Overlay({ zIndex = 1_000, backgroundColor, opacity, onClick }: Props) {
   return (
     <div
       style={{
         position: 'fixed',
         inset: 0,
-        zIndex: zIndex
+        zIndex: zIndex,
+        backgroundColor: backgroundColor ?? '#000',
+        opacity: opacity ?? 0.15
       }}
       onClick={onClick}
     />
